2def23389d3deed3c2071842af6b683f330419e5
Murtuza Rangwala <murtuza.rangwala@1rivet.com>
2026-01-29 18:04:36 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="todo-python-app",
+    version="0.1.0",
+    author="Murtuza Rangwala",
+    description="My first Python demo package",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
